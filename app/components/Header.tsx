6c68b16844972d61d95b048add94e6c6d7675fee
ibrahim6755 <ibrahimghani020@gmail.com>
2023-08-10 19:58:28 -0700

    changes

---
+++ app/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const links = ["About", "Services", "Work", "Contact"]
+
+
+export default function Header() {
+  return (
+    <div className="header-wrapper flex align-middle justify-between p-4 bg-slate-200 drop-shadow-md">
+      <div className="left-nav">
+        <div className="brand-name">
+          <Link href="/"><h1 className='ml-8 font-bold text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  '>Ibrahim</h1></Link>
+        </div>
+      </div>
+      <div className="right-nav">
+        <ul className='flex align-center justify-start gap-6'>
+          {
+            links.map((link) => (
+              <li className='list-none font-semibold hover:bg-blue-500 active:bg-blue-600 active:ring-black focus:outline-none focus:ring focus:ring-blue-700 p-1 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '>
+                <Link href={link.toLowerCase()}>{link}</Link>
+              </li>
+            ))
+          }
+
+        </ul>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,68 @@
+"use client"
+import React from 'react'
+import { useEffect, useRef } from 'react'
 import Image from 'next/image'
+import Typed from 'typed.js'
+import sec1img from './imgs/sec-1.png'
+
+
 
 export default function Home() {
+  // Create reference to store the DOM element containing the animation
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['WEB DEVELOPER', 'UI/UX DEVELOPER.',],
+      typeSpeed: 60,
+      loop: true, // Add this option to enable continuous looping
+    });
+
+    // No need for cleanup function to stop the animation
+
+    // Optional: If you want to stop the animation on component unmount, keep the cleanup function
+    // return () => {
+    //   typed.destroy();
+    // };
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section className='header-section flex items-center justify-center mb-3 mt-16'>
+        <div className="info  items-center justify-center w-2/4">
+          <h2 className='font-bold text-3xl mr-2 whitespace-normal leading-10'>Welcome I am  <span className='text-blue-600 font-bold'>Ibrahim Ghani</span>.<br /> I live in Pakistan.I make Fully Responsive Designs & I am a <span className='text-blue-600' ref={el} /></h2>
+          <div className="links ">
+          <ul className='flex items-center'>
+            <li className='animate-bounce w-10 h-10 motion-safe:animate-bounce m-1 mt-12 list-none font-semibold hover:bg-blue-500 active:bg-blue-600 active:ring-black focus:outline-none focus:ring focus:ring-blue-700 p-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'><a href=""><img className='w-6' src="https://www.svgrepo.com/show/494202/facebook.svg" alt="" /></a></li>
+            <li className='animate-bounce w-10 h-10 motion-safe:animate-bounce m-1 mt-12 list-none font-semibold hover:bg-blue-500 active:bg-blue-600 active:ring-black focus:outline-none focus:ring focus:ring-blue-700 p-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'><a href=""><img className='w-6' src="https://www.svgrepo.com/show/500461/whatsapp.svg" alt="" /></a></li>
+            <li className='animate-bounce w-10 h-10 motion-safe:animate-bounce m-1 mt-12 list-none font-semibold hover:bg-blue-500 active:bg-blue-600 active:ring-black focus:outline-none focus:ring focus:ring-blue-700 p-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'><a href=""><img className='w-6' src="https://www.svgrepo.com/show/494217/twitter.svg" alt="" /></a></li>
+            <li className='animate-bounce w-10 h-10 motion-safe:animate-bounce m-1 mt-12 list-none font-semibold hover:bg-blue-500 active:bg-blue-600 active:ring-black focus:outline-none focus:ring focus:ring-blue-700 p-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'><a href=""><img className='w-6' src="https://www.svgrepo.com/show/453774/instagram.svg" alt="" /></a></li>
+          </ul>
         </div>
-      </div>
+        </div>
+        <div className="img">
+          <Image src={sec1img} alt="Main img" />
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className='about mt-16'>
+        <div className="heading flex items-center justify-center ">
+          <h2 className='font-bold text-4xl'>About Me</h2>
+        </div>
+        <div className="paragaph flex items-center align-middle justify-center mt-9 ">
+          <p className='text-center w-9/12 leading-9'>Hello! I'm <span className='text-blue-600 font-bold'>Ibrahim Ghani</span> , a passionate and innovative software developer with a love for turning ideas into elegant, functional solutions. With a strong foundation in <span className='text-blue-600 font-bold'>HTML,CSS,JS,REACT,BOOTSTRAP,NEXT JS , TAILWIND CSS</span> , I thrive on tackling complex challenges and creating user-centric applications that make a real impact.
+            Collaboration and communication are at the heart of my work. I enjoy collaborating with cross-functional teams, designers, and stakeholders to ensure every project is not only technically sound but also aligns seamlessly with the overall vision. I'm a firm believer in the power of clean code, elegant design, and intuitive user experiences.
+            Feel free to explore my portfolio to get a glimpse of some of the exciting projects I've had the privilege to work on. If you're interested in collaborating or have a project in mind, don't hesitate to reach out. I'm always excited about new opportunities to create meaningful and impactful solutions.</p>
+        </div>
+      </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="skills-wrapper">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+    </>
+
+
   )
 }
